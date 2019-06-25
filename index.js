31338f6c29e179b4f86a43c1db7e1e0275b5a719
Warpfox <tylermwatts@gmail.com>
2019-06-25 07:08:29 -0500

    specify origin

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const routeConfig = require('./routes');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://warpfox-schedule-client.herokuapp.com/'
+  );
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
